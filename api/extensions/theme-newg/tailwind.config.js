20cb6a8e1f02e1b96477ea2e7361f53d3fa3a9c5
Audrius <65190196+boo-code@users.noreply.github.com>
2025-01-04 00:25:12 +0100

    load full tw and daisy

--- api/extensions/theme-newg/tailwind.config.js
+++ api/extensions/theme-newg/tailwind.config.js
@@ -3,11 +3,20 @@ export default {
   content: [
     "./src/**/*.{js,ts,vue}",
   ],
+  safelist: [
+    // Preload all color variations
+    {
+      pattern: /.+/, // This will match all classes
+      variants: ['hover', 'focus', 'active', 'disabled', 'first', 'last', 'odd', 'even', 
+                'sm', 'md', 'lg', 'xl', '2xl',
+                'dark', 'light']
+    }
+  ],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
   daisyui: {
     themes: ["light", "dark"],
-  },
+  }
 }
